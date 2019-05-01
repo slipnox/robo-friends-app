3803d429c69949f61aa1c5220662cc74092bd7ab
Abrahan Silverio <abrahansmg@gmail.com>
2019-05-01 02:21:35 -0400

    fix standard format

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='profile-picture' src='https://robohash.org/test?200x200'/>
+      <img alt='profile-picture' src='https://robohash.org/test?200x200' />
       <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
