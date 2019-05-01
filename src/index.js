b895b5ba085c6462a573b583466a51ad13b31250
Abrahan Silverio <abrahansmg@gmail.com>
2019-05-01 02:20:09 -0400

    clean extra files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import Card from './Card.js'
+
 import './index.css'
 import 'tachyons'
 
-import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<div>
+  <Card />
+  <Card />
+  <Card />
+</div>
+, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
