0cfc573fee33470a19a9d5083ef2e5821cbf463d
Abrahan Silverio <abrahansmg@gmail.com>
2019-05-01 03:31:06 -0400

    added search box functionlity

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import CardList from './CardList'
+import SearchBox from './SearchBox'
+import { robots } from './robots'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField : event.target.value })
+  }
+
+  render () {
+    const filteredFrobots = this.state.robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
+    return (
+      <div className='tc'>
+        <h1>Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredFrobots} />
+      </div>
+    )
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div className='tc'>
+//       <h1>Robofriends</h1>
+//       <SearchBox />
+//       <CardList robots={state.robots} />
+//     </div>
+//   )
+// }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Card from './Card.js'
-import { robots }  from './robots'
-
 import './index.css'
 import 'tachyons'
+import App from './App'
 
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<div>
-  <Card name={robots[0].name} email={robots[0].email} id={robots[0].id} />
-  <Card name={robots[1].name} email={robots[1].email} id={robots[1].id} />
-  <Card name={robots[2].name} email={robots[2].email} id={robots[2].id} />
-</div>
-, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
