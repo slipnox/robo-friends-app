830129784092481ed3ef534fe982a205f55ba7fc
Abrahan Silverio <abrahansmg@gmail.com>
2019-05-01 02:02:45 -0400

    moved hello component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'tachyons'
 
-// import App from './App'
-import Hello from './Hello'
+import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Hello greeting={`React Ninja`} />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
